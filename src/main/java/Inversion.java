74f67d0a058bd1a33ddfedfdd790223a5eecd888
Brayan Stiven Ochoa Botero <brayan8abo@outlook.es>
2024-02-24 18:35:59 +0100

    Creacion de inversion y metodos

---
+++ src/main/java/Inversion.java
@@ -0,0 +1,31 @@
+package main.java;
+
+public class Inversion {
+
+	private String nombre;
+	private double cantidad;
+
+	// Constructor
+	public Inversion(String nombre, double cantidad) {
+		this.nombre = nombre;
+		this.cantidad = cantidad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(double cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+
+}
